38f18339ce8ea92cb6f0ac60c6c0ca60a4e37790
José Araújo <jaraujo@adm.ua.pt>
2022-07-21 14:39:36 +0100

    segunda versão do código

---
+++ ConsoleThreeQuestionSurvey/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleThreeQuestionSurvey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int age = 0;
+            string month = "";
+            
+            Console.WriteLine("Waht is your name ? ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Waht is your age ? ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Waht is your birth month ? ");
+            month = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0}, you are {1} years old and yor birth month is {2} .", name, age, month);
+
+
+        }
+    }
+}
